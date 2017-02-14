316a5a4f086627f5aae7c0defdca84d9d0f1c159
Florian Breisch <florian.breisch@mindkeeper.solutions>
2017-02-14 12:33:57 +0100

    Initial draft

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+const chalk = require('chalk')
+
+const keySeparator = '.'
+const keyValueSeparator = ':'
+const coloredKeySeparator = chalk.blue(keySeparator)
+const coloredKeyValueSeparator = chalk.gray(keyValueSeparator)
+
 function keyMax(object) {
 	Object.keys(object).reduce((l, v) => Math.max(l, v), 0)
 }
 
-function stringify(indentation, object) {
-	const indnt = Number.isInteger(indentation) && indentation > 0 ? indentation : 0
+function stringify(object, prefix, level) {
+	if (level === undefined) {
+		level = 0
+	}
+	if (prefix === undefined) {
+		prefix = ''
+	}
 
 	let result = ''
-	Object.keys(object).forEach(k => {
-		if (result.length > 0) {
-			result += ' '.repeat(indnt)
+	let keyIndex = -1
+	Object.keys(object).forEach(key => {
+		if (level > 0) {
+			keyIndex += 1
 		}
-		result += `${k}`
 
-		if (typeof object[k] === 'object' && !Array.isArray(object[k])) {
-			result += '.'
-			result += stringify(indnt + k.length + 1, object[k])
+		if (result.length > 0 ||
+			level <= 0) {
+			result += chalk.gray(prefix)
+			if (keyIndex >= 0 && level > 0) {
+				result = result.concat(chalk.gray(keySeparator))
+			}
+		}
+
+		result += chalk.blue(key)
+		const value = object[key]
+
+		if (typeof value === 'object') {
+			if (Object.keys(value).length > 0) {
+				const newPrefix = level > 0 ? prefix.concat(keySeparator, key) : prefix.concat(key)
+				result += chalk.blue(keySeparator)
+				result += stringify(value, newPrefix, level + 1)
+			} else {
+				if (Array.isArray(value)) {
+					result = result.concat(keyValueSeparator, ' <none>')
+				} else {
+					result = result.concat(keyValueSeparator, ' <none>')
+				}
+				result += `\n`
+			}
 		} else {
-			result += '\n'
+			result = result.concat(keyValueSeparator, ' ', value, '\n')
 		}
 	})
 
@@ -24,5 +57,5 @@ function stringify(indentation, object) {
 }
 
 module.exports = function(object) {
-	return stringify(0, object)
+	return stringify(object, '  ')
 }
---
+++ readme.md
@@ -0,0 +1,29 @@
+<h1><pre>
+           .──────◯──────────────.
+     _.─◯─'◦  ◯  ◦   ◯  ◦  ◯   ◯ ◡`─◌──.
+  ,─___◡  __◌_  __◦ __ ◡  ___◦  __◦_  __'_.
+ ╱ (  _ \(  _ \(  )(  ( \(_  _)(  __)(  ( \╲
+(   ) __/ )   / )( /    /  )(   ) _) /    / )
+▒`.(__)  (__\_)(__)\_)__) (__) (____)\_)__)'▒
+ ▒▒'─. ◌  ◦   ◯ ◡   ◦ ◡  ◯ ◌   ◯     ◌ ,─'▒▒
+   ▒▒▒`────◯ ◡   ◯   ◌     ◡ ◦   _.◯──'▒▒▒
+      ▒▒▒▒▒▒`─◦─────────◯──────◯'▒▒▒▒▒▒
+            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+</pre></h1>
+
+> Print your objects nice and tasty
+
+## Why
+
+* Printed JSON objects should be easy readable by humans
+* Printed JSON objects should be easy readable by robots
+
+## Install
+
+## Usage
+
+## API
+
+## License
+
+MIT © [Florian Breisch]()
